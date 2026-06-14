00ac5d0e6e7878c89b2980ba6f7e5c645f672c8a
interpunktus <240490702+interpunktus@users.noreply.github.com>
2026-06-14 19:10:33 +0300

    try1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,21 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
-  // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+wall.addEventListener('click', (e) => {
+  const wallRect = wall.getBoundingClientRect();
+
+  const spiderWidth = spider.offsetWidth;
+  const spiderHeight = spider.offsetHeight;
+
+  let left = e.clientX - wallRect.left - spiderWidth / 2;
+  let spiderTop = e.clientY - wallRect.top - spiderHeight / 2;
+
+  left = Math.max(0, Math.min(left, wallRect.width - spiderWidth));
+
+  spiderTop = Math.max(0, Math.min(spiderTop, wallRect.height - spiderHeight));
+
+  spider.style.left = `${left}px`;
+  spider.style.top = `${spiderTop}px`;
 });
